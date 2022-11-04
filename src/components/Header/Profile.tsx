4cf2c69eb2bced8e29a700dd5c2c82daa7dbb146
Diego Ramires <goncalves.ramires@gmail.com>
2022-11-04 09:46:25 -0300

    update: create individual files as header components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,23 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+interface ProfileProps {
+  name: string,
+  email: string
+}
+
+export function Profile({ name, email}: ProfileProps) {
+  return (
+    <Flex align='center'>
+      <Box mr='4' textAlign='right'>
+        <Text>{name}</Text>
+        <Text color='gray.300' fontSize='small'>{email}</Text>
+      </Box>
+
+      <Avatar
+        size='md'
+        name='Diego Gonçalves'
+        src='https://github.com/diegoramires.png'
+      />
+    </Flex>
+  )
+}
