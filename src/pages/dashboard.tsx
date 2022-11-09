326e0bb3dc6d3f73a4b738f7e413d0f74d08b1ee
Diego Ramires <goncalves.ramires@gmail.com>
2022-11-09 11:36:12 -0300

    update: mobile padding adjust on dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -72,7 +72,7 @@ export default function Dashboard() {
 
         <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems='flex-start'>
           <Box
-            p='8'
+            p={['6', '8']}
             bg='gray.800'
             borderRadius={8}
             pb='4'
@@ -81,7 +81,7 @@ export default function Dashboard() {
             <Chart series={series} options={options} height={160} type="area" />
           </Box>
           <Box
-            p='8'
+            p={['6', '8']}
             bg='gray.800'
             borderRadius={8}
             pb='4'
